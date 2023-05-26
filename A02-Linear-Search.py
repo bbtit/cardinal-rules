22e2ac5da19e6def9fad2cd5ce9edc31d0c40a29
bbtit <the8butterfly8dream@gmail.com>
2023-05-27 00:17:18 +0900

    A02

---
+++ A02-Linear-Search.py
@@ -0,0 +1,14 @@
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+ans = False
+
+for i in A:
+    if i == X:
+        ans = True
+        break
+    
+if ans:
+    print("Yes")
+else:
+    print("No")
