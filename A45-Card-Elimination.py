b656336355a413827b4be0f6d028b7c33e15e598
bbtit <the8butterfly8dream@gmail.com>
2023-09-02 10:47:10 +0900

    A45

---
+++ A45-Card-Elimination.py
@@ -0,0 +1,21 @@
+N, C = input().split()
+N = int(N)
+A = input()
+
+score = 0
+for i in range(N):
+    if A[i] == "W":
+        score += 0
+    if A[i] == "B":
+        score += 1
+    if A[i] == "R":
+        score += 2
+
+if score % 3 == 0 and C == "W":
+    print("Yes")
+elif score % 3 == 1 and C == "B":
+    print("Yes")
+elif score % 3 == 2 and C == "R":
+    print("Yes")
+else:
+    print("No")
